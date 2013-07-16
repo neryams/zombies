11de605d70dda36e55d61183962c99e78b57d0ce
Raymon Ohmori <neryams@gmail.com>
2013-07-16 15:36:05 -0700

    Fixed human resistance not working at all.

--- js/modules/resistance.js
+++ js/modules/resistance.js
@@ -3,17 +3,17 @@
 */
 new Module('infect', function(current,target,strength) {
 	// If this is running  due to an upgrade, do the upgrade
-	if(current.id == target.id && current.total_pop > 0) {
+	if(current.total_pop > 0 && current.cache) {
 		// .cache property contains the last turn's (or however many turns ago it changed) population data.
 		if(current.cache.total_pop != current.total_pop)
 			current.human_strength *= (current.total_pop/current.cache.total_pop)
 
-		var civilians = current.total_pop
-		if(current.army) {
-			civilians -= army.size;
-		}
 		// Civilians have a base strength of 0.01 per person, soldiers have experience (starting at 1)
-		var adjustedCombat = current.human_strength * civilians * 0.01 + army.size * army.experience;
+		if(current.army) {
+			var adjustedCombat = current.human_strength * (current.total_pop - current.army.size) * 0.01 + current.army.size * current.army.experience;
+		} else {
+			var adjustedCombat = current.human_strength * current.total_pop * 0.01;
+		}
 		adjustedCombat *= 0.1;
 		strength.kill /= (1+adjustedCombat);
 		strength.infectSelf /= (1+adjustedCombat);
