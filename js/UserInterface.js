5cefcf82c194028b7e982683c08b9c99c3245276
Raymon Ohmori <neryams@gmail.com>
2014-03-18 18:00:27 -0700

    Change date printing to use i18n

--- js/UserInterface.js
+++ js/UserInterface.js
@@ -12,18 +12,10 @@ function bind(scope, fn) {
 	};
 }
 Date.prototype.getMonthName = function(lang) {
-	lang = lang && (lang in Date.locale) ? lang : 'en';
-	return Date.locale[lang].month_names[this.getMonth()];
+	return i18n.t('ui:dates.month_names.'+this.getMonth());
 };
 Date.prototype.getMonthNameShort = function(lang) {
-	lang = lang && (lang in Date.locale) ? lang : 'en';
-	return Date.locale[lang].month_names_short[this.getMonth()];
-};
-Date.locale = {
-	en: {
-	   month_names: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-	   month_names_short: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-	}
+	return i18n.t('ui:dates.month_names_short.'+this.getMonth());
 };
 
 function DataField(id,type,options,parent) {
