2caf40ba3f56d56033e37eab8a18d614a7960321
Raymon Ohmori <neryams@gmail.com>
2014-05-25 15:11:57 -0700

    Boats have 'movement points' like hordes now

--- js/modules/shipping/seaMove.js
+++ js/modules/shipping/seaMove.js
@@ -5,27 +5,32 @@ exports.type = 'event';
 exports.run = function() {
 	for(var i = 0, n = this.boats.length; i < n; i++) {
 		var boat = this.boats[i];
-		
-		this.S.UILink.updateHorde('boat', boat);
 
-		if(!boat.advance()) {
+		if(boat.route.length === 0) {
 			this.S.UILink.updateHorde('boat', boat, true);
 			this.boats.splice(i, 1);
 			n--;
 			i--;
+		} else {
+			boat.movePoints += this.boatSpeed;
+			boat.advance();
+			this.S.UILink.updateHorde('boat', boat);
 		}
 	}
 };
 exports.options = {
 	init: function() {
-		var boatCount = 0,
+		var UILink = this.S.UILink,
+			boatCount = 0,
 			boatInitCount = 10;
 		this.boats = [];
+		this.boatSpeed = 100;
 
 		this.S.UILink.addNewHordeType('boat', boatInitCount, {
 			iconSizeMin: 20,
 			iconSizeMax: 20,
-			opacity: 1
+			opacity: 1,
+			randomize: false
 		});
 
 		this.addNew = function(path) {
@@ -38,13 +43,23 @@ exports.options = {
 			this.route = route.reverse();
 			this.size = size;
 
-			this.advance();
+			this.location = this.route.pop();
+			UILink.updateHorde('boat', this);
 		};
 		Boat.prototype = {
 			size: 0,
+			movePoints: 0,
 			advance: function() {
-				this.location = this.route.pop();
+				while(this.movePoints > this.nextPointDistance()) {
+					this.movePoints -= this.nextPointDistance();
+					this.location = this.route.pop();
+					if(this.route.length === 0)
+						break;
+				}
 				return this.location;
+			},
+			nextPointDistance: function() {
+				return this.location.getDistanceTo(this.route[this.route.length - 1]);
 			}
 		};
 	}
