f10a1d18ff8754ea66bdfb3c609e792a4f422994
Raymon Ohmori <neryams@gmail.com>
2014-06-03 17:07:56 -0700

    Refactor panic event module

--- js/modules/panic/events.js
+++ js/modules/panic/events.js
@@ -4,45 +4,61 @@
 exports.type = 'event';
 exports.run = function() {
 	if(this.S.status.panic > 0) {
+		var thresholds = this.countryPanicThresholds;
 		// Check country panic levels, level up is panic is higher than the defined threshold
 		for(var i = 1; i < this.S.countries.length; i++) {
-			if(this.S.countries[i].panic > 0) {
-				if(this.S.countries[i].panic > this.countryPanicThresholds[this.S.countries[i].currPanicLevel]) {
-					// If the panic level is past the maximum, turn off the growth.
-					if(this.countryPanicThresholds.length > this.S.countries[i].currPanicLevel) {
-						this.addCountryNews(this.S.countries[i].currPanicLevel, this.S.countries[i].name);
-						this.S.countries[i].currPanicLevel++;
-					}
-					else {
-						this.addCountryNews('end', this.S.countries[i].name);
-						this.S.countries[i].currPanicLevel = 0;
-					}
-				}
+			if(this.S.countries[i].panicLevel !== 'max') {
+				var panic = this.S.countries[i].panic;
+				var panicLevel = this.S.countries[i].panicLevel;
+
+				if(panic > thresholds[panicLevel + 1])
+					this.raisePanic(this.S.countries[i]);
 
 				// Clamp panic above minimum for the level
-				if(this.S.countries[i].panic < this.countryPanicThresholds[this.S.countries[i].currPanicLevel - 1]) {
-					this.S.countries[i].panic = this.countryPanicThresholds[this.S.countries[i].currPanicLevel - 1];
-				}
-			}
+				panic = Math.max(panic, thresholds[panicLevel]);
 
-			// Panic to display is progress to next level, so (panic increase since last level up) / (difference between this level and next level)
-			this.S.status['country_panic-'+i] = (this.S.countries[i].panic - this.countryPanicThresholds[this.S.countries[i].currPanicLevel - 1]) / (this.countryPanicThresholds[this.S.countries[i].currPanicLevel] - this.countryPanicThresholds[this.S.countries[i].currPanicLevel - 1]);
+				// Panic to display is progress to next level, so (panic increase since last level up) / (difference between this level and next level)
+				this.S.status['country_panic-'+i] = (panic - thresholds[panicLevel]) / (thresholds[panicLevel + 1] - thresholds[panicLevel]);
+			}
 		}
 	}
 
 };
 exports.options = {
 	init: function () {
-		this.addCountryNews = function(panicLevel, name) {
-			this.S.UILink.addNews('country_military.' + panicLevel, [name]);
-		};
-
 		for(var i = 1; i < this.S.countries.length; i++) {
-			this.S.countries[i].currPanicLevel = 1;
+			this.S.countries[i].panicLevel = 0;
+			this.S.countries[i].panic = 0;
 		}
-		this.panicThresholds = [0,10];
-		this.currPanicLevel = 1;
-		this.countryPanicThresholds = [0,10,5000,160000,320000];
+		this.countryPanicThresholds = [
+			0,
+			10,
+			500,
+			10000
+		];
+
+		var panicFunctions = [
+			null,
+			function(country) {
+
+			},
+			function(country) {
+
+			},
+			function(country) {
+
+			}
+		];
+		this.raisePanic = function(country) {
+			country.panicLevel++;
+
+			this.S.UILink.addNews('country_military.' + country.panicLevel, [country.name]);
+			panicFunctions[country.panicLevel](country);
+
+			// If panic level is maxed, make sure you don't add any more
+			if(this.countryPanicThresholds[country.panicLevel + 1] === undefined)
+				country.panicLevel = 'max';
+		};
 	},
 	ui: function(UI) {
 		var mainInfo = UI.interfaceParts.main_info;
